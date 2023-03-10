9a6bfc0139bf4671f13fae6c1f611cd2cfb03987
Sapronov Konstantin <ks.costas@gmail.com>
2023-03-10 15:00:20 +0300

    feat: add 404 page

---
+++ src/pages/404/NotFoundPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class NotFoundPage extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1 className="text-center">404 Not found!</h1>
+      </div>
+    );
+  }
+}
