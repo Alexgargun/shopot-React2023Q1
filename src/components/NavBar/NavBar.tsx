62c9974eb7d41ee6bccf2ed9dee95ddfc5da11a8
Sapronov Konstantin <ks.costas@gmail.com>
2023-03-10 14:57:23 +0300

    feat: add nav bar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { TypeRoute } from 'shared/types';
 import styles from './NavBar.module.scss';
 
 export class NavBar extends React.Component {
   render(): React.ReactNode {
     return (
       <nav className={styles.nav}>
-        <u className={styles.menu}>
-          <li className={styles.menuItem}>
-            <a href="/">Main</a>
-          </li>
-          <li>
-            <a href="/about">About Us</a>
-          </li>
-        </u>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? `${styles.navLink} ${styles.navLinkActive}` : styles.navLink
+          }
+          to={TypeRoute.Main}
+        >
+          Main
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? `${styles.navLink} ${styles.navLinkActive}` : styles.navLink
+          }
+          to={TypeRoute.About}
+        >
+          About Us
+        </NavLink>
       </nav>
     );
   }
