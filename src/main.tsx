4e2db363d0fbf09856c14dd1f41f6ee2590c953e
Sapronov Konstantin <ks.costas@gmail.com>
2023-03-10 14:56:28 +0300

    feat: remove StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,4 @@ import App from './app';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
