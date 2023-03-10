23a5da47f59660831bcb4e89f5cf88a68c4cf944
Sapronov Konstantin <ks.costas@gmail.com>
2023-03-10 15:00:36 +0300

    feat: add about page

---
+++ src/pages/about/AboutPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class AboutPage extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1 className="text-center">Hello, this is About Us page!</h1>
+      </div>
+    );
+  }
+}
