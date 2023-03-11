8eaea163b4b0bc3df356e51a52c7807d4cbd6864
Sapronov Konstantin <ks.costas@gmail.com>
2023-03-11 15:14:37 +0300

    feat: add message No data

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -28,6 +28,10 @@ export class CardList extends React.Component<CardListProps, CardListState> {
   }
 
   render(): React.ReactNode {
+    if (this.state.cards.length === 0) {
+      return <div className="text-center">No data</div>;
+    }
+
     return (
       <div className={styles.cardList}>
         {this.state.cards.map((card: ICard) => {
