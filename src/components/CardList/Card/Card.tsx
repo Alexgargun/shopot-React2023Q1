d0fe11b657b4f515546d0f233c897b504589d0a5
Sapronov Konstantin <ks.costas@gmail.com>
2023-03-11 12:34:46 +0300

    feat: add styles

--- src/components/CardList/Card/Card.tsx
+++ src/components/CardList/Card/Card.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './Card.module.scss';
 import { ICard } from 'shared/interfaces';
+import { EyeOutlined, LikeOutlined } from '@ant-design/icons';
 
 interface ICardProps {
   card: ICard;
@@ -25,11 +26,19 @@ export class Card extends React.Component<ICardProps, ICardState> {
         </div>
         <div className={styles.body}>
           <div className={styles.title}>{this.state.title}</div>
-          <div className={styles.by}>{this.state.owner}</div>
+          <div className={styles.by}>
+            Added by <span>{this.state.owner}</span>
+          </div>
           <div className={styles.info}>
             <div className={styles.stats}>
-              <span>{this.state.views}</span>
-              <span>{this.state.likes}</span>
+              <span>
+                <EyeOutlined className={styles.statsIcon} style={{ color: 'gray' }} />
+                {this.state.views}
+              </span>
+              <span>
+                <LikeOutlined className={styles.statsIcon} style={{ color: 'gray' }} />
+                {this.state.likes}
+              </span>
             </div>
             <div className={styles.addedAt}>{this.state.addedAt}</div>
           </div>
