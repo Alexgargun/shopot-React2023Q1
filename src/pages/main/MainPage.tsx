cab03350e753207c18f1fcd77d2219794646ef1e
Sapronov Konstantin <ks.costas@gmail.com>
2023-03-12 12:03:01 +0300

    refactor: add preload page from CardList

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -2,18 +2,41 @@ import { CardList } from 'components/CardList';
 import { MotionContainer } from 'components/MotionContainer';
 import { SearchBar } from 'components/SearchBar';
 import React from 'react';
+import { ICard } from 'shared/interfaces';
 import cardService from 'shared/services/cardService';
 
-const cards = cardService.findAll();
+interface IMainPageState {
+  cards: ICard[];
+}
 
-export class MainPage extends React.Component {
+export class MainPage extends React.Component<object, IMainPageState> {
+  constructor(props: object) {
+    super(props);
+
+    this.state = {
+      cards: [],
+    };
+  }
+
+  componentDidMount(): void {
+    const cards: ICard[] = cardService.findAll();
+
+    // Load images
+    cards.forEach((card) => {
+      const img = new Image();
+      img.src = card.image;
+    });
+
+    this.setState({ cards: cards });
+  }
+
   render() {
     return (
       <div className="container">
         <SearchBar />
         <h1 className="text-center">YouTube API Websites</h1>
         <MotionContainer identKey="cardList">
-          <CardList cards={cards} />
+          <CardList cards={this.state.cards} />
         </MotionContainer>
       </div>
     );
