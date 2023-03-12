c2a233836134330a4bf3be462c46ba1e0b80baee
Sapronov Konstantin <ks.costas@gmail.com>
2023-03-12 10:41:38 +0300

    feat: add test for CardList component

---
+++ src/components/CardList/CardList.test.tsx
@@ -0,0 +1,47 @@
+import { render, screen } from '@testing-library/react';
+import { it, describe, expect } from 'vitest';
+
+import { ICard } from 'shared/interfaces';
+import { CardList } from './CardList';
+
+const cardListMock: ICard[] = [
+  {
+    id: 1,
+    title: 'Mock Object 1',
+    owner: 'MERCIV',
+    image: '/images/image-01.webp',
+    views: 400,
+    likes: 399,
+    addedAt: '20/05/2022',
+  },
+  {
+    id: 2,
+    title: 'Mock Object 2',
+    owner: 'MERCIV',
+    image: '/images/image-02.webp',
+    views: 300,
+    likes: 299,
+    addedAt: '21/05/2022',
+  },
+];
+
+/**
+ * @vitest-environment jsdom
+ */
+describe('CardList component', () => {
+  it('CardList renders', () => {
+    render(<CardList cards={cardListMock} />);
+    expect(screen.getByText('Mock Object 1')).toBeInTheDocument();
+    expect(screen.getByText('Mock Object 2')).toBeInTheDocument();
+  });
+
+  it('CardList renders without items', () => {
+    render(<CardList cards={[]} />);
+    expect(screen.getByText('No data')).toBeInTheDocument();
+  });
+
+  it('CardList snapshot', () => {
+    const result = render(<CardList cards={cardListMock} />);
+    expect(result).toMatchSnapshot();
+  });
+});
