6d0a14901a8458e22f758ec3d1e1b622bd97fce1
Sapronov Konstantin <ks.costas@gmail.com>
2023-03-13 14:43:46 +0300

    feat: add createNavBar

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,16 +6,27 @@ import { MainPage } from 'pages/main';
 import { NotFoundPage } from 'pages/404';
 import { AboutPage } from 'pages/about';
 import { Footer } from 'components/Footer';
+import { NavBar } from 'components/NavBar';
 
 class App extends React.Component {
+  createNavBar(headerText: string) {
+    return <NavBar headerText={headerText} />;
+  }
+
   render() {
     return (
       <div className="App">
         <BrowserRouter>
           <Routes>
-            <Route path={TypeRoute.Main} element={<MainPage />} />
-            <Route path={TypeRoute.About} element={<AboutPage />} />
-            <Route path="*" element={<NotFoundPage />} />
+            <Route
+              path={TypeRoute.Main}
+              element={<MainPage>{this.createNavBar('Main')}</MainPage>}
+            />
+            <Route
+              path={TypeRoute.About}
+              element={<AboutPage>{this.createNavBar('About Us')}</AboutPage>}
+            />
+            <Route path="*" element={<NotFoundPage>{this.createNavBar('404')}</NotFoundPage>} />
           </Routes>
           <Footer />
         </BrowserRouter>
