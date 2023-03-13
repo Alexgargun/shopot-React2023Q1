6d5ebca0a69a94d207cb35fe69f55df6d48512dc
Sapronov Konstantin <ks.costas@gmail.com>
2023-03-13 17:05:27 +0300

    feat: add StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,8 @@ import App from './app';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
