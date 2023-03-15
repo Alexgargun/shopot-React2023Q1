69b37a6f2d2ccc428425b48a38d20826d7b0910f
Sapronov Konstantin <ks.costas@gmail.com>
2023-03-15 15:37:29 +0300

    feat: add test for NavBar

---
+++ src/components/NavBar/NavBar.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+
+import { it, describe, expect } from 'vitest';
+import { NavBar } from './NavBar';
+
+const text = 'NavBar text for header';
+
+/**
+ * @vitest-environment jsdom
+ */
+describe('NavBar component without props', () => {
+  it('NavBar renders', async () => {
+    render(
+      <BrowserRouter>
+        <NavBar headerText={text} />
+      </BrowserRouter>
+    );
+
+    expect(screen.getByText(text)).toBeInTheDocument();
+
+    const links = screen.getAllByRole('link');
+
+    expect(links[0]).toHaveTextContent(/Main/i);
+    expect(links[1]).toHaveTextContent(/About Us/i);
+  });
+});
