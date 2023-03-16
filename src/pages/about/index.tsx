a597e72c0a42aa361d862d86bc156f2648779f3d
Sapronov Konstantin <ks.costas@gmail.com>
2023-03-16 19:50:00 +0300

    refactor: change export path for about

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export class AboutPage extends React.Component {
+export default class AboutPage extends React.Component {
   render() {
     return (
       <div className="container">
