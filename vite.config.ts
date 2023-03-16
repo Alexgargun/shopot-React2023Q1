3209874b1426b193cd1222966fad7a94708a0e1c
Sapronov Konstantin <ks.costas@gmail.com>
2023-03-16 18:58:35 +0300

    feat: add alias for src

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,16 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
