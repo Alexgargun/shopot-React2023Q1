a80d02b08cbfefdaf1122a8b033df5d62ebbde0f
Sapronov Konstantin <ks.costas@gmail.com>
2023-03-16 20:05:22 +0300

    refactor: implement get routes form array

--- src/widgets/navbar/ui/index.tsx
+++ src/widgets/navbar/ui/index.tsx
@@ -1,6 +1,6 @@
+import { routes } from 'pages';
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { TypeRoute } from 'shared/types';
 import styles from './styles.module.scss';
 
 interface INavbarProps {
@@ -13,18 +13,23 @@ export default class Navbar extends React.Component<INavbarProps> {
   }
 
   render(): React.ReactNode {
+    const navLinks = routes.map((route) => {
+      return (
+        <NavLink
+          key={route.key}
+          className={({ isActive }) => this.getLinkStyle(isActive)}
+          to={route.path}
+        >
+          {route.title}
+        </NavLink>
+      );
+    });
+
     return (
       <header className="container">
         <div className={styles.innerHeader}>
           <div className={styles.headerText}>{this.props.headerText}</div>
-          <nav className={styles.nav}>
-            <NavLink className={({ isActive }) => this.getLinkStyle(isActive)} to={TypeRoute.Main}>
-              Main
-            </NavLink>
-            <NavLink className={({ isActive }) => this.getLinkStyle(isActive)} to={TypeRoute.About}>
-              About Us
-            </NavLink>
-          </nav>
+          <nav className={styles.nav}>{navLinks}</nav>
         </div>
       </header>
     );
