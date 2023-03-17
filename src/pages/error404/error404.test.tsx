c1ef6882abb012c5784c1c34b023f16ffa9fe1a5
Sapronov Konstantin <ks.costas@gmail.com>
2023-03-17 06:41:01 +0300

    feat: add test for error404 component

---
+++ src/pages/error404/error404.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import Error404 from '.';
+
+/**
+ * @vitest-environment jsdom
+ */
+describe('Error 404 page test', () => {
+  it('Error 404 page render', () => {
+    render(<Error404 />);
+    expect(screen.getByText('404 Not found')).toBeInTheDocument();
+  });
+});
