8f0d80b3b1312f982bff787e9cfc80fd227d812b
Sapronov Konstantin <ks.costas@gmail.com>
2023-03-17 17:57:40 +0300

    feat: add render test

--- src/widgets/page-container/page-container.test.tsx
+++ src/widgets/page-container/page-container.test.tsx
@@ -1,14 +1,18 @@
-import { render } from '@testing-library/react';
 import { FC } from 'react';
-import { it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
 import PageContainer from './ui';
 
+const matchH1Text = 'Test page';
+
 const MockPage: FC = () => {
-  return <h1>Test page</h1>;
+  return <h1>{matchH1Text}</h1>;
 };
 
 describe('PageContainer test', () => {
   it('PageContainer render test', () => {
-    render(<PageContainer headerText="Test page" content={<MockPage />} routes={[]} />);
+    render(<PageContainer headerText="Header text" content={<MockPage />} routes={[]} />);
+
+    expect(screen.getByText(matchH1Text)).toBeInTheDocument();
   });
 });
