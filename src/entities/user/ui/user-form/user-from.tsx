937e580f43d89a8e4ebbbbe0e8d60e5ad37263ae
Sapronov Konstantin <ks.costas@gmail.com>
2023-03-20 10:00:44 +0300

    feat: add user from component

---
+++ src/entities/user/ui/user-form/user-from.tsx
@@ -0,0 +1,139 @@
+import styles from './styles.module.scss';
+import { Component, createRef, FormEvent, ReactNode, RefObject } from 'react';
+import { Button } from 'shared/ui';
+import {
+  dateOfBirthValidator,
+  emailValidator,
+  FieldDateOfBirth,
+  FieldEmail,
+  FieldFullName,
+  FieldPreferCity,
+  fullNameValidator,
+  positionValidator,
+  preferCityValidator,
+  programmingWithValidator,
+} from 'entities/user';
+import { initialUserFrom } from './user-form.initial';
+import FieldProgramming from './field-programming';
+import FieldPosition from './field-position';
+import { debounce } from 'shared/helpers';
+
+interface IUserFormsState {
+  data: typeof initialUserFrom.data;
+  errors: typeof initialUserFrom.errors;
+}
+
+type TypeUserFormProps = Record<string, never>;
+
+export default class UserForm extends Component<TypeUserFormProps, IUserFormsState> {
+  private static UPDATE_DELAY = 500;
+  private formRef: RefObject<HTMLFormElement>;
+  private isFirstSubmit: boolean;
+  private debounceTimeout: { timeout: ReturnType<typeof setTimeout> | null };
+
+  constructor(props: TypeUserFormProps) {
+    super(props);
+
+    this.formRef = createRef<HTMLFormElement>();
+    this.isFirstSubmit = true;
+    this.state = { data: { ...initialUserFrom.data }, errors: { ...initialUserFrom.errors } };
+    this.debounceTimeout = { timeout: null };
+  }
+
+  handleFormChange(): void {
+    if (!this.isFirstSubmit) {
+      debounce(() => this.validateForm(), this.debounceTimeout, UserForm.UPDATE_DELAY);
+    }
+  }
+
+  resetFrom(): void {
+    this.formRef.current?.reset();
+    this.setState({ data: { ...initialUserFrom.data }, errors: { ...initialUserFrom.errors } });
+    this.isFirstSubmit = true;
+  }
+
+  updateFieldValue(data: { [key: string]: string | string[] }) {
+    this.setState((prevState: IUserFormsState) => ({
+      data: { ...prevState.data, ...data },
+    }));
+  }
+
+  validateForm(): boolean {
+    const { fullName, email, dateOfBirth, preferCity, programming, position } = this.state.data;
+
+    const errors = {
+      fullName: fullNameValidator(fullName),
+      email: emailValidator(email),
+      dateOfBirth: dateOfBirthValidator(dateOfBirth),
+      preferCity: preferCityValidator(preferCity),
+      programming: programmingWithValidator(programming),
+      position: positionValidator(position),
+    };
+
+    this.setState({ errors: errors });
+
+    return Array.from(Object.values(errors)).reduce((res: boolean, error: string) => {
+      return error !== '' ? false : res;
+    }, true);
+  }
+
+  handleFormSubmit(e: FormEvent): void {
+    e.preventDefault();
+    this.isFirstSubmit = false;
+
+    if (this.validateForm()) {
+      console.log('this.validateForm() = true');
+    }
+  }
+
+  render(): ReactNode {
+    return (
+      <div className="container">
+        <div className={styles.formWrapper}>
+          <form
+            ref={this.formRef}
+            onSubmit={(e: FormEvent) => this.handleFormSubmit(e)}
+            onChange={() => this.handleFormChange()}
+          >
+            <div className={styles.formRow}>
+              <FieldFullName
+                error={this.state.errors.fullName}
+                onChange={(value) => this.updateFieldValue({ fullName: value })}
+              />
+              <FieldEmail
+                error={this.state.errors.email}
+                onChange={(value) => this.updateFieldValue({ email: value })}
+              />
+            </div>
+            <div className={styles.formRow}>
+              <FieldDateOfBirth
+                error={this.state.errors.dateOfBirth}
+                onChange={(value) => this.updateFieldValue({ dateOfBirth: value })}
+              />
+              <FieldPreferCity
+                error={this.state.errors.preferCity}
+                onChange={(value) => this.updateFieldValue({ preferCity: value })}
+              />
+            </div>
+            <div className={styles.formRow}>
+              <FieldProgramming
+                error={this.state.errors.programming}
+                onChange={(value) => this.updateFieldValue({ programming: value })}
+              />
+              <FieldPosition
+                error={this.state.errors.position}
+                onChange={(value) => this.updateFieldValue({ position: value })}
+              />
+            </div>
+            <div className={`${styles.formRow} flex-center`}>
+              <Button variant="primary">Send</Button>
+              <Button type="button" onClick={() => this.resetFrom()}>
+                Reset
+              </Button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
