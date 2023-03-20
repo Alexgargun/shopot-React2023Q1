a55e51c5c0e3f96849b9348a8b9e15bbe6a24d7a
Sapronov Konstantin <ks.costas@gmail.com>
2023-03-20 09:59:35 +0300

    feat: add initial data for user form

---
+++ src/entities/user/ui/user-form/user-form.initial.ts
@@ -0,0 +1,18 @@
+export const initialUserFrom = {
+  data: {
+    fullName: '',
+    email: '',
+    dateOfBirth: '',
+    preferCity: '',
+    programming: [],
+    position: '',
+  },
+  errors: {
+    fullName: '',
+    email: '',
+    dateOfBirth: '',
+    preferCity: '',
+    programming: '',
+    position: '',
+  },
+};
