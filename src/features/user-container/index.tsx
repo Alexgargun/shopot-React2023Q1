94065199564cccd8b209df4392b96590686dca77
Sapronov Konstantin <ks.costas@gmail.com>
2023-03-21 18:26:56 +0300

    feat: implement  message add data successfully

--- src/features/user-container/index.tsx
+++ src/features/user-container/index.tsx
@@ -1,23 +1,33 @@
 import { Component, ReactNode } from 'react';
 import { TypeUser, UserCardList, UserForm } from 'entities/user';
+import AlertMessage from 'widgets/alert-message';
 
 interface IUserContainerState {
   users: TypeUser[];
+  isAlertShowed: boolean;
 }
 export default class UserContainer extends Component<Record<string, never>, IUserContainerState> {
   constructor(props: Record<string, never>) {
     super(props);
 
-    this.state = { users: [] };
+    this.state = { users: [], isAlertShowed: false };
   }
 
   addUser(user: TypeUser) {
-    this.setState((prevState: IUserContainerState) => {
-      const users = [...prevState.users];
-      users.push({ ...user, id: `${Date.now()}` });
+    this.setState(
+      (prevState: IUserContainerState) => {
+        const users = [...prevState.users];
+        users.push({ ...user, id: `${Date.now()}` });
 
-      return { users: users };
-    });
+        return { users: users };
+      },
+      () => this.showAlertMessage()
+    );
+  }
+
+  showAlertMessage() {
+    this.setState({ isAlertShowed: true });
+    setTimeout(() => this.setState({ isAlertShowed: false }), 4000);
   }
 
   render(): ReactNode {
@@ -25,6 +35,7 @@ export default class UserContainer extends Component<Record<string, never>, IUse
       <>
         <UserForm addUser={(user: TypeUser) => this.addUser(user)} />
         <UserCardList users={this.state.users} />
+        {this.state.isAlertShowed && <AlertMessage message="User data added successfully." />}
       </>
     );
   }
