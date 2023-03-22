2b14c6bce508aa4138d40ecffa2e1c18bb42373d
Sapronov Konstantin <ks.costas@gmail.com>
2023-03-22 19:54:47 +0300

    refactor: remove todo

--- src/features/user-container/user-container.test.tsx
+++ src/features/user-container/user-container.test.tsx
@@ -1,6 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import { cleanup, render, screen } from '@testing-library/react';
 import { describe, expect, it, vi } from 'vitest';
 import { UserContainer } from '.';
+
+afterEach(() => {
+  cleanup();
+});
 
 vi.mock('../../entities/user/ui/user-form/user-from', () => {
   const UserForm = vi.fn(() => <div>UserFormMock</div>);
@@ -18,27 +22,5 @@ describe('Test render UserContainer', () => {
   it('Test render UserContainer', () => {
     expect(screen.getByText('UserFormMock')).toBeInTheDocument();
     expect(screen.getByText('UserCardListMock')).toBeInTheDocument();
-  });
-});
-
-describe.todo('Test UserContainer addUser method', () => {
-  const str = JSON.stringify([{ name: 'teresa teng' }]);
-  const blob = new Blob([str]);
-  const file = new File([blob], 'values.json', {
-    type: 'image/jpeg',
-  });
-
-  const mockUser = {
-    fullName: 'Demo Demo',
-    email: 'mail@mail.com',
-    dateOfBirth: '2007-01-02',
-    preferCity: 'London',
-    programming: ['React'],
-    position: 'Junior',
-    avatar: file,
-  };
-
-  it('Test UserContainer addUser method', () => {
-    expect(mockUser).toBe(mockUser);
   });
 });
