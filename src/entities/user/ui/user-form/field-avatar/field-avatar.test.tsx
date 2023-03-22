50f14c63351664ab60319ebb274dd9f23dfd68ee
Sapronov Konstantin <ks.costas@gmail.com>
2023-03-22 10:03:11 +0300

    feat: add test

---
+++ src/entities/user/ui/user-form/field-avatar/field-avatar.test.tsx
@@ -0,0 +1,17 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { TypeFormValue } from 'entities/user';
+import { describe, it, vi } from 'vitest';
+import FieldAvatar from '.';
+
+const mockOnChange = vi.fn((value: TypeFormValue) => value);
+
+describe('Test FieldAvatar', () => {
+  it('Test render FieldAvatar', () => {
+    render(<FieldAvatar error="" onChange={mockOnChange} />);
+
+    const input = screen.getByLabelText('input-avatar') as HTMLInputElement;
+
+    fireEvent.change(input, { target: { value: '' } });
+    expect(input).toHaveValue('');
+  });
+});
