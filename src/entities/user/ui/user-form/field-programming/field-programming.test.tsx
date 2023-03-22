9e67826e3ad4ccd31ec0ac800a0ab6e5cac29f4a
Sapronov Konstantin <ks.costas@gmail.com>
2023-03-22 09:47:05 +0300

    feat: add test

---
+++ src/entities/user/ui/user-form/field-programming/field-programming.test.tsx
@@ -0,0 +1,19 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { TypeFormValue } from 'entities/user';
+import { describe, it, vi } from 'vitest';
+import FieldProgramming from '.';
+
+const mockOnChange = vi.fn((value: TypeFormValue) => value);
+
+describe('Test FieldProgramming', () => {
+  it('Test set value', () => {
+    render(<FieldProgramming error="" onChange={mockOnChange} />);
+
+    const input = screen.getByLabelText('input-programming-React') as HTMLInputElement;
+
+    expect(input).not.toBeChecked();
+    input.click();
+    expect(input).toBeChecked();
+    expect(mockOnChange).toBeCalledTimes(1);
+  });
+});
